fa1fa374867ed351d10984316a0f5ae3f5067077
worldofmarcus <me@marcuseriksson.com>
2022-09-08 09:00:08 +0000

    Add add categories

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     // sidenav initialization
-    let sidenav = document.querySelectorAll('.sidenav');
+    let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
 });
